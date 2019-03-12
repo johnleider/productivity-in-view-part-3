573d2137cd72068f36d7f3bce9f24d5f3fee1c4d
John Leider <john.j.leider@gmail.com>
2019-03-12 11:02:37 -0500

    feat(Toolbar): add mock visuals

--- src/components/core/Toolbar.vue
+++ src/components/core/Toolbar.vue
@@ -4,6 +4,27 @@
     color="white"
     prominent
   >
-    <v-toolbar-title>App</v-toolbar-title>
+    <v-avatar
+      color="grey"
+      class="mr-3"
+    />
+
+    <v-btn
+      v-for="n in 4"
+      :key="n"
+      flat
+    >
+      Item {{ n }}
+    </v-btn>
+
+    <v-spacer />
+
+    <v-flex xs4>
+      <v-text-field
+        solo-inverted
+        flat
+        hide-details
+      />
+    </v-flex>
   </v-toolbar>
 </template>
